d1917626f1cc38e19c85f841c73b97671bc9d87b
nipf1078@colorado.edu <Nicholas.Pfeufer@colorado.edu>
2017-08-02 19:59:41 -0600

    clean for merge amazon build

---
+++ CitizenSkyView/app/src/main/java/edu/boulder/citizenskyview/citizenskyview/SkyViewEvent.java
@@ -0,0 +1,40 @@
+package edu.boulder.citizenskyview.citizenskyview;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+/**
+ * Created by Nicholas on 7/20/2017.
+ */
+@DynamoDBTable(tableName = "Events")
+class SkyViewEvent {
+
+    private String eventName;
+    private String end;
+    private String start;
+
+    @DynamoDBIndexRangeKey(attributeName = "EventName")
+    public String getEvetName() {
+        return eventName;
+    }
+
+    public void setEventName(String name) {
+        this.eventName = name;
+    }
+
+
+    @DynamoDBAttribute(attributeName = "End")
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @DynamoDBAttribute(attributeName = "Start")
+    public String getStart() {
+        return start;
+    }
+
+    public void setHardCover(String start) {
+        this.start = start;
+    }
+}
